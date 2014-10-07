ff31fe7ea6133c636b73df45bb63fd14d1e697f4
Αλέξανδρος Παπαγεωργίου <oriong85@gmail.com>
2014-10-07 16:16:44 +0300

    Namespace problem 2.
    Solution functions don't need to be static when they are namespaced
    as they don't clash with eachother.

--- problem2/problem2.cpp
+++ problem2/problem2.cpp
@@ -1,13 +1,16 @@
 #include "../solutions.h"
 #include "../stream.h"
 
-static long solution(){
+namespace euler{
+	namespace problem2{
+
+long solution(){
 	long result = 0;
 	//Create a fibonacci number stream...
-	euler::FibonacciStream fs(1, 1);
+	FibonacciStream fs(1, 1);
 
 	// and keep only the even ones that are smaller than 4000000.
-	for(euler::EvenFilter ef2(&fs); *ef2 < 4000000; ef2.next())
+	for(EvenFilter ef2(&fs); *ef2 < 4000000; ef2.next())
 	{
 		//std::cout << *ef2 << ", ";
 		//add those numbers to form the result.
@@ -18,3 +21,7 @@ static long solution(){
 }
 
 RegisterSolution(2, solution);
+
+	} //problem2
+} //euler
+

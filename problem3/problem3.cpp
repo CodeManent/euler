0b65b906ba66a1e889e47bc7afc14c01b73928a9
Αλέξανδρος Παπαγεωργίου <oriong85@gmail.com>
2014-10-07 16:32:20 +0300

    Namespace problem 3.

--- problem3/problem3.cpp
+++ problem3/problem3.cpp
@@ -3,17 +3,16 @@
 #include "../stream.h"
 #include <algorithm>
 
+namespace euler{
+	namespace problem3{
+
 /******************************************************************************
  * Finds the max factor of the given number.
  * First it gets all its factors and then scans them to find the max.
  */
-static long solution1(){
+long solution1(){
 	long num = 600851475143L;
-	auto f = euler::primeFactors(num);
-
-	//for(auto n: f)
-	//	std::cout << n << ", ";
-	using namespace euler;
+	auto f = primeFactors(num);
 
 	auto maxFactor = std::max_element(f.begin(), f.end());
 	return *maxFactor;
@@ -28,10 +27,10 @@ RegisterSolution(3, solution1);
  * The second solution uses the same method to find the solution with the
  * difference that it is implemented around number streams.
  */
-static long solution2(){
+long solution2(){
 	long num = 600851475143L;
 
-	euler::PrimeFactorStream pfs(num);
+	PrimeFactorStream pfs(num);
 	long maxFactor = 0;
 	for(; !pfs.isEmpty(); pfs.next()){
 		if(maxFactor < *pfs)
@@ -43,4 +42,7 @@ static long solution2(){
 }
 
 RegisterSolution(3, solution2);
+
+	} //problem3
+} //euler
 
